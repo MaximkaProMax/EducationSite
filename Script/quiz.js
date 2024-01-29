8f652e280a12df0d875dbad889871d134d08ad8b
Maxim <Zholdashev.Maks@yandex.ru>
2024-01-29 19:06:31 +0300

    Add pages

--- Script/quiz.js
+++ Script/quiz.js
@@ -4,10 +4,10 @@ const resultsDiv = document.querySelector('#results');
 quizForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const answers = ['a', 'b'];
+    const answers = ['a', 'a', 'a', 'c', 'a', 'b', 'c', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'];
     let score = 0;
 
-    for (let i = 1; i <= answers.length; i++) {
+    for (let i = 1; i <= questions.length; i++) {
         const answer = event.target[`q${i}`].value;
 
         if (answer === answers[i - 1]) {
@@ -15,7 +15,7 @@ quizForm.addEventListener('submit', (event) => {
         }
     }
 
-    const percentage = Math.round(score / answers.length * 100);
+    const percentage = Math.round(score / questions.length * 100);
 
-    resultsDiv.innerHTML = `Вы набрали ${score} из ${answers.length} (${percentage}%) правильных ответов.`;
+    resultsDiv.innerHTML = `Вы набрали ${score} из ${questions.length} (${percentage}%) правильных ответов.`;
 });
