62df9f95f44a37ebf75eb56757fe8b86aee63672
Max <Zholdashev.Maks@yandex.ru>
2024-01-29 20:25:05 +0300

    MoreQuiz

---
+++ Script/QuizLang.js
@@ -0,0 +1,21 @@
+const quizForm = document.querySelector('#quiz');
+const resultsDiv = document.querySelector('#results');
+
+quizForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const answers = ['a', 'b'];
+    let score = 0;
+
+    for (let i = 1; i <= answers.length; i++) {
+        const answer = event.target[`q${i}`].value;
+
+        if (answer === answers[i - 1]) {
+            score++;
+        }
+    }
+
+    const percentage = Math.round(score / answers.length * 100);
+
+    resultsDiv.innerHTML = `Вы набрали ${score} из ${answers.length} (${percentage}%) правильных ответов.`;
+});
